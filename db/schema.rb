b9b45681823780530db74d878547758f4c270968
Adalberto GONZALEZ <adalg08@gmail.com>
2017-06-20 15:22:24 +0200

    delete column ingredients from recipes

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170620091248) do
+ActiveRecord::Schema.define(version: 20170620132031) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "recipes", force: :cascade do |t|
     t.string   "name"
-    t.text     "ingredients"
     t.text     "description"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
