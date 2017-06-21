f6b7de1d277f1f5b3271ccf7e1252d2f43a415c5
Adalberto GONZALEZ <adalg08@gmail.com>
2017-06-21 16:53:55 +0200

    CRUD recipes done

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,6 +1,31 @@
 class RecipesController < ApplicationController
 
   def index
-    @recipes = Recipe.all
+    @recipes = current_user.recipes.all
+  end
+
+  def show
+    @recipe = Recipe.find(params[:id])
+  end
+
+  def new
+    @recipe = Recipe.new
+  end
+
+  def create
+    @recipe = current_user.recipes.new(recipe_params)
+    @recipe.user = current_user
+    if @recipe.save
+      flash[:notice] = "Recette sauvegardée"
+      redirect_to @recipe
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def recipe_params
+    params.require(:recipe).permit(:name, :description)
   end
 end
