4262a40059c7660b24a57b45a37585d51d9a4e61
Adalberto GONZALEZ <adalg08@gmail.com>
2017-06-29 16:00:02 +0200

    validations and associations added to category  and recipe

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,6 @@
 class Category < ApplicationRecord
   validates :name, presence: true
 
+  has_many :groups
+  has_many :recipes, through: :groups
 end
--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -4,5 +4,7 @@ class Recipe < ApplicationRecord
   belongs_to :user
   has_many :compositions, dependent: :destroy
   has_many :ingredients, through: :compositions
+  has_many :groups
+  has_many :categories, through: :groups
   has_attachment :photo
 end
