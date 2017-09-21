1c6e95a453005cde20209a0aa1e23aeebe5ca61b
Adalberto Gonzalez <adalg08@gmail.com>
2017-09-21 18:38:21 +0200

    current user recipes fixed

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -2,7 +2,7 @@ class RecipesController < ApplicationController
   before_action :set_recipe, only: [:show, :edit, :update, :destroy]
 
   def index
-      @recipes = Recipe.all.order(id: :desc)
+      @recipes = current_user.recipes.all.order(id: :desc)
   end
 
   def search_results
