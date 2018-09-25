4c9564f1e2e062406b081a2f427a3346d09fc689
Adalberto Gonzalez <adalg08@gmail.com>
2018-09-25 22:11:29 +0200

    uniqueness deleted

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,5 +1,5 @@
 class Recipe < ApplicationRecord
-  validates :name, presence: true, uniqueness: true
+  validates :name, presence: true
 
   belongs_to :user
   has_many :compositions, dependent: :destroy
